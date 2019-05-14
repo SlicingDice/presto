5240064d5338e8e94055d142141d391e934ea91a
Fernando Werneck <fernando.barros@nerus.com.br>
2019-05-14 09:36:49 -0300

    Access Connector

---
+++ presto-access/src/main/java/io/prestosql/plugin/csv/AccessClient.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.plugin.csv;
+
+import com.slicingdice.drivers.SlicingDiceCsvDriver;
+import com.slicingdice.drivers.SlicingDiceGoogleDriveDriver;
+import io.prestosql.plugin.jdbc.BaseJdbcClient;
+import io.prestosql.plugin.jdbc.BaseJdbcConfig;
+import io.prestosql.plugin.jdbc.ConnectionFactory;
+import io.prestosql.plugin.jdbc.DriverConnectionFactory;
+import io.prestosql.spi.type.StandardTypes;
+import io.prestosql.spi.type.Type;
+import io.prestosql.spi.type.TypeManager;
+import io.prestosql.spi.type.TypeSignature;
+
+import javax.inject.Inject;
+
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.Properties;
+
+import static io.prestosql.plugin.jdbc.DriverConnectionFactory.basicConnectionProperties;
+
+public class AccessClient
+        extends BaseJdbcClient
+{
+    protected final Type jsonType;
+
+    @Inject
+    public AccessClient(BaseJdbcConfig config, TypeManager typeManager)
+            throws SQLException
+    {
+        super(config, "\"", connectionFactory(config));
+        this.jsonType = typeManager.getType(new TypeSignature(StandardTypes.JSON));
+    }
+
+    private static ConnectionFactory connectionFactory(BaseJdbcConfig config)
+            throws SQLException
+    {
+        Properties connectionProperties = basicConnectionProperties(config);
+
+        return new DriverConnectionFactory(new SlicingDiceGoogleDriveDriver(), config.getConnectionUrl(),
+                Optional.ofNullable(config.getUserCredentialName()),
+                Optional.ofNullable(config.getPasswordCredentialName()),
+                connectionProperties);
+    }
+}
